c833835608710d65da91ac686902f38e3a0c34cb
Panagiotis Kostolias <pkostolias@dousiscom.gr>
2026-02-24 18:11:40 +0200

    test commit

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,24 @@
+require('dotenv').config();
+
 const express = require('express');
 const axios = require('axios');
 
 const app = express();
 const PORT = 3000;
 
-const WP_BASE_URL = 'https://www.youweekly.gr/wp-json/wp/v2/posts';
+const WP_BASE_URL = process.env.WP_BASE_URL;
+const WP_USERNAME = process.env.WP_USERNAME;
+const WP_PASSWORD = process.env.WP_PASSWORD;
+
+const authHeader = 'Basic ' + Buffer.from(`${WP_USERNAME}:${WP_PASSWORD}`).toString('base64');
 
 app.get('/posts', async (req, res) =>{
     try {
-        const response = await axios.get(`${WP_BASE_URL}/POSTS`);
+        const response = await axios.get(`${WP_BASE_URL}/POSTS`, {
+            headers: {
+                Authorization: authHeader
+            }
+        });
         res.json(response.data);
     } catch (error) {
         res.status(500).json({ error: 'Failed to fetch posts' });
