b4bad73bf7d83cfb9663189e6dd43baf623bc082
Panagiotis Kostolias <pkostolias@dousiscom.gr>
2026-02-25 17:57:00 +0200

    react app commit

--- mobile-app/src/api/apiClient.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const API_BASE_URL = 'http://localhost:3000';
-
-export const fetchPosts = async () => {
-    const response = await axios.get(`${API_BASE_URL}/posts`);
-    return response.data;
-};
